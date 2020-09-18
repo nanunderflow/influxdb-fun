d2d576e492ddfb29ea3387a87cd16abca299eaaa
fitz <nanunderflow@gmail.com>
2020-09-17 21:16:25 -0400

    wip

--- main.rb
+++ main.rb
@@ -2,9 +2,11 @@ require 'influxdb'
 
 influxdb = InfluxDB::Client.new 'metrics'
 
+i = rand(0..10)
+
 data = {
-  values: { value: 5 },
-  tags: { operation: 'write' },
+  values: { value: i },
+  tags: { operation: "write_" + i.to_s },
   timestamp: Time.now.to_i # timestamp is optional, if not provided point will be saved with current time
 }
 
@@ -15,4 +17,8 @@ influxdb.query 'select * from foobar group by operation' do |name, tags, points|
   points.each do |pt|
     printf "  -> %p\n", pt
   end
+end
+
+influxdb.list_databases.each do |database|
+  puts database["name"]
 end
